f73717d67652a5a8c0a4c5c2510ec80ed8b97c9d
christoffersylve <christoffer.sylve@outlook.com>
2025-09-09 10:27:02 +0200

    Added more error handling

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,5 +1,5 @@
 // app/api/search/route.ts
-import { UnsplashImage } from "@/types/unsplash";
+import { unsplashColors, UnsplashImage } from "@/types/unsplash";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
@@ -8,18 +8,32 @@ export async function GET(req: Request) {
   const query = searchParams.get("q");
   const perPage = searchParams.get("per_page") || "24";
   const page = searchParams.get("page") || "1";
-  const color = searchParams.get("color") || ""; // Currently not used
+  const color = searchParams.get("color"); 
 
+  
 
   if (!query) {
     console.log("Missing query parameter");
     return NextResponse.json({ error: "Missing query" }, { status: 400 });
+  } else {
+    params.set("query", query);
+  }
+  
+  
+  const perPage_ = Number(perPage)
+  if (!isNaN(perPage_) && perPage_ >= 1 && perPage_ <= 30) {
+    params.set("per_page", perPage.toString());
+  } else {
+    params.set("per_page", "24"); 
   }
 
-  if (query) params.set("query", query);
-  if (perPage) params.set("per_page", perPage.toString());
   if (page) params.set("page", page.toString());
-  if (color) params.set("color", color);
+
+  if (color&&(unsplashColors.some(({ id }) => id === color))) {
+    params.set("color", color);
+  } else {
+    return NextResponse.json({ error: `Invalid color:${color}` }, { status: 400 });
+  }
 
   try {
 
